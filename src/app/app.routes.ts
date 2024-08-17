8a36e9d6fb6177d65a1705cdd7feb5b702864c0a
Sechaba Mache <93662067+sechaba-mache@users.noreply.github.com>
2024-08-18 01:16:07 +0200

    Add login component and authguard for home page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,15 @@
 import { Routes } from '@angular/router';
+import { AuthGuard } from './guards/auth-guard.guard';
 
 export const routes: Routes = [
   {
     path: 'home',
     loadComponent: () => import('./home/home.page').then((m) => m.HomePage),
+    canActivate: [AuthGuard],
+  },
+  {
+    path: 'login',
+    loadComponent: () => import('./login/login.component').then((m) => m.LoginComponent),
   },
   {
     path: '',
--- src/main.ts
+++ src/main.ts
@@ -4,11 +4,13 @@ import { IonicRouteStrategy, provideIonicAngular } from '@ionic/angular/standalo
 
 import { routes } from './app/app.routes';
 import { AppComponent } from './app/app.component';
+import { AuthGuard } from './app/guards/auth-guard.guard';
 
 bootstrapApplication(AppComponent, {
   providers: [
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
     provideIonicAngular(),
     provideRouter(routes, withPreloading(PreloadAllModules)),
+    AuthGuard
   ],
 });
