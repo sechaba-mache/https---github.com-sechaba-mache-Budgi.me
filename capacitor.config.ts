76f041ed08a59588780b4d7f594289b7b597f788
Sechaba Mache <93662067+sechaba-mache@users.noreply.github.com>
2024-08-18 00:18:34 +0200

    Init App and add scripts to run native applications

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.budgime.app',
+  appName: 'Budgi.me',
+  webDir: 'www'
+};
+
+export default config;
