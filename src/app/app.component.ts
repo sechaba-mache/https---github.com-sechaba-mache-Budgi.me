df63e645abbb6fa8746aeb69321e4df8dc50271e
Sechaba Mache <93662067+sechaba-mache@users.noreply.github.com>
2024-08-18 04:24:38 +0200

    Style login page (issues on native)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { IonApp, IonRouterOutlet } from '@ionic/angular/standalone';
+import { IonicModule } from '@ionic/angular'
 
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
   standalone: true,
-  imports: [IonApp, IonRouterOutlet],
+  imports: [IonApp, IonRouterOutlet, IonicModule],
 })
 export class AppComponent {
   constructor() {}
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,15 +1,15 @@
 import { Component, OnInit } from '@angular/core';
+import { IonicModule } from '@ionic/angular'
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss'],
+  imports: [IonicModule],
   standalone: true
 })
-export class LoginComponent  implements OnInit {
+export class LoginComponent {
 
   constructor() { }
-
-  ngOnInit() {}
 
 }
