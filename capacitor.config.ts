e5ea5fd00481792b15a700fcf0313195e23d1873
Sechaba Mache <93662067+sechaba-mache@users.noreply.github.com>
2024-08-18 12:42:36 +0200

    Config update

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'com.budgime.app',
+  appId: 'io.ionic.starter',
   appName: 'Budgi.me',
   webDir: 'www'
 };
