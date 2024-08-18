df9871a129bdf5ba18fa52ba587e9f4e692aeb03
Sechaba Mache <93662067+sechaba-mache@users.noreply.github.com>
2024-08-18 11:55:31 +0200

    Remove ionic module from app.component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
 import { IonApp, IonRouterOutlet } from '@ionic/angular/standalone';
-import { IonicModule } from '@ionic/angular'
 
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
   standalone: true,
-  imports: [IonApp, IonRouterOutlet, IonicModule],
+  imports: [IonApp, IonRouterOutlet],
 })
 export class AppComponent {
   constructor() {}
