b07c3ba4f1ce507d034e267afbaf10d5d29e6b7f
Sechaba Mache <93662067+sechaba-mache@users.noreply.github.com>
2024-08-31 13:21:18 +0200

    Add routing for receipt page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,8 +4,12 @@ import { AuthGuard } from './guards/auth-guard.guard';
 export const routes: Routes = [
   {
     path: 'home',
-    loadComponent: () => import('./home/home.page').then((m) => m.HomePage),
+    loadComponent: () => import('./home/home-page/home.page').then((m) => m.HomePage),
     canActivate: [AuthGuard],
+  },
+  {
+    path: 'receipt/:id',
+    loadComponent: () => import('./home/receipt/receipt.component').then((m) => m.ReceiptComponent),
   },
   {
     path: 'login',
