6efa707b8d49baac2446f21f81b544dcc769ec0d
Sechaba Mache <93662067+sechaba-mache@users.noreply.github.com>
2024-08-31 19:44:38 +0200

    Finalise receipt page styling

--- src/app/home/receipt/receipt.component.ts
+++ src/app/home/receipt/receipt.component.ts
@@ -1,15 +1,16 @@
 import { Component, OnInit } from '@angular/core';
+import { IonContent } from '@ionic/angular/standalone';
+
 
 @Component({
   selector: 'app-receipt',
   templateUrl: './receipt.component.html',
   styleUrls: ['./receipt.component.scss'],
+  imports: [IonContent],
   standalone: true
 })
-export class ReceiptComponent  implements OnInit {
+export class ReceiptComponent {
 
   constructor() { }
-
-  ngOnInit() {}
 
 }
